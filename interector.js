cb05ecfb35a3aada327dc9281e21ac78f04fd5bc
César Damasceno <64749161+ManeMaria@users.noreply.github.com>
2020-06-08 22:26:28 -0300

    clone_keycode
    Clone of kaycode.info for study purposes

---
+++ interector.js
@@ -0,0 +1,11 @@
+let showNumber =(()=>{
+    let numberDisplay = document.querySelector("#numberDisplay");
+    let letterDisplay = document.querySelector("#areaMsg")
+    let postClickCode = document.querySelector("#postClickCode"); 
+    
+    document.addEventListener("keydown",(tecla)=>{
+        letterDisplay.innerHTML = ''
+        numberDisplay.textContent = tecla.keyCode;
+        postClickCode.textContent= String.fromCharCode(tecla.keyCode);
+    })
+})();
